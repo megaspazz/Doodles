b851a1579926ff45577347c0d61afc373ee755f0
Eric Pan <erzpan@gmail.com>
2022-02-06 23:32:40 -0800

    Comment out document logging again.

--- Mingle/log.js
+++ Mingle/log.js
@@ -9,17 +9,16 @@ class Log {
             console.log(...arguments)
         }
 
-        if (Log._DOCUMENT) {
-            if (!Log.divOutput) {
-                Log.divOutput = document.createElement("div");
-                Log.divOutput.style.fontFamily = "monospace";
-                document.getElementsByTagName("body")[0].appendChild(Log.divOutput);
-            }
-            let divLine = document.createElement("div");
-            // divLine.innerText = "[ " + new Date(Date.now()).toLocaleString() + " ]\n" + [...arguments].join("\n");
-            divLine.innerText = "wrote it";
-            divLine.style.margin = "6px";
-            Log.divOutput.appendChild(divLine);
-        }
+        // if (Log._DOCUMENT) {
+        //     if (!Log.divOutput) {
+        //         Log.divOutput = document.createElement("div");
+        //         Log.divOutput.style.fontFamily = "monospace";
+        //         document.getElementsByTagName("body")[0].appendChild(Log.divOutput);
+        //     }
+        //     let divLine = document.createElement("div");
+        //     divLine.innerText = "[ " + new Date(Date.now()).toLocaleString() + " ]\n" + [...arguments].join("\n");
+        //     divLine.style.margin = "6px";
+        //     Log.divOutput.appendChild(divLine);
+        // }
     }
 }
