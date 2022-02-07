8ddef89f7cd059a204382d4824d3c0a40854c8cc
Eric Pan <erzpan@gmail.com>
2022-02-06 19:39:54 -0800

    Add more detailed logging.

--- Mingle/log.js
+++ Mingle/log.js
@@ -1,9 +1,21 @@
 class Log {
-    static #DEBUG = true;
+    static #CONSOLE = true;
+    static #DOCUMENT = true;
 
     static write() {
-        if (Log.#DEBUG) {
+        if (Log.#CONSOLE) {
             console.log(...arguments)
+        }
+
+        if (Log.#DOCUMENT) {
+            if (!Log.divOutput) {
+                Log.divOutput = document.createElement("div");
+                Log.divOutput.style.fontFamily = "monospace";
+                document.getElementsByTagName("body")[0].appendChild(Log.divOutput);
+            }
+            let divLine = document.createElement("div");
+            divLine.innerText = "[ " + new Date(Date.now()).toLocaleString() + " ]\n" + [...arguments].join("\n");
+            Log.divOutput.appendChild(divLine);
         }
     }
 }
