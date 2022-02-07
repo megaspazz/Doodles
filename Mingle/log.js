02b24912d809035bea54d8e25b595a61d288441f
Eric Pan <erzpan@gmail.com>
2022-02-07 01:22:57 -0800

    Move Log.write declaration out of class.

--- Mingle/log.js
+++ Mingle/log.js
@@ -3,22 +3,22 @@ class Log {
     static _DOCUMENT = true;
 
     static divOutput = null;
+}
 
-    static write() {
-        if (Log._CONSOLE) {
-            console.log(...arguments)
-        }
+Log.write = function() {
+    if (Log._CONSOLE) {
+        console.log(...arguments)
+    }
 
-        if (Log._DOCUMENT) {
-            if (!Log.divOutput) {
-                Log.divOutput = document.createElement("div");
-                Log.divOutput.style.fontFamily = "monospace";
-                document.getElementsByTagName("body")[0].appendChild(Log.divOutput);
-            }
-            let divLine = document.createElement("div");
-            divLine.innerText = "[ " + new Date(Date.now()).toLocaleString() + " ]\n" + [...arguments].join("\n");
-            divLine.style.margin = "6px";
-            Log.divOutput.appendChild(divLine);
+    if (Log._DOCUMENT) {
+        if (!Log.divOutput) {
+            Log.divOutput = document.createElement("div");
+            Log.divOutput.style.fontFamily = "monospace";
+            document.getElementsByTagName("body")[0].appendChild(Log.divOutput);
         }
+        let divLine = document.createElement("div");
+        divLine.innerText = "[ " + new Date(Date.now()).toLocaleString() + " ]\n" + [...arguments].join("\n");
+        divLine.style.margin = "6px";
+        Log.divOutput.appendChild(divLine);
     }
 }
