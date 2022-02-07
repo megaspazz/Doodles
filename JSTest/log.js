eed60e03914727abb741eb97036e38c5074fdf11
Eric Pan <erzpan@gmail.com>
2022-02-06 21:26:38 -0800

    No more importing ES6 class.

--- JSTest/log.js
+++ JSTest/log.js
@@ -1,10 +1,3 @@
-function log() {
-    let divLine = document.createElement("div");
-    divLine.innerText = "[ " + new Date(Date.now()).toLocaleString() + " ]\n" + [...arguments].join("\n");
-    divLine.style.padding = "6px";
-    document.getElementsByTagName("body")[0].appendChild(divLine);
-}
-
 class Log {
     static #CONSOLE = false;
     static #DOCUMENT = false;
