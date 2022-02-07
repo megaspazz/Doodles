ecceba74927ed30c6f3cd037233b501e7a2138aa
Eric Pan <erzpan@gmail.com>
2022-02-06 23:10:40 -0800

    Include Log._CONSOLE portion.

--- Mingle/log.js
+++ Mingle/log.js
@@ -2,21 +2,21 @@ class Log {
     static _CONSOLE = true;
     static _DOCUMENT = true;
 
-    // static write() {
-    //     if (Log._CONSOLE) {
-    //         console.log(...arguments)
-    //     }
+    static write() {
+        if (Log._CONSOLE) {
+            console.log(...arguments)
+        }
 
-    //     if (Log._DOCUMENT) {
-    //         if (!Log.divOutput) {
-    //             Log.divOutput = document.createElement("div");
-    //             Log.divOutput.style.fontFamily = "monospace";
-    //             document.getElementsByTagName("body")[0].appendChild(Log.divOutput);
-    //         }
-    //         let divLine = document.createElement("div");
-    //         divLine.innerText = "[ " + new Date(Date.now()).toLocaleString() + " ]\n" + [...arguments].join("\n");
-    //         divLine.style.margin = "6px";
-    //         Log.divOutput.appendChild(divLine);
-    //     }
-    // }
+        // if (Log._DOCUMENT) {
+        //     if (!Log.divOutput) {
+        //         Log.divOutput = document.createElement("div");
+        //         Log.divOutput.style.fontFamily = "monospace";
+        //         document.getElementsByTagName("body")[0].appendChild(Log.divOutput);
+        //     }
+        //     let divLine = document.createElement("div");
+        //     divLine.innerText = "[ " + new Date(Date.now()).toLocaleString() + " ]\n" + [...arguments].join("\n");
+        //     divLine.style.margin = "6px";
+        //     Log.divOutput.appendChild(divLine);
+        // }
+    }
 }
